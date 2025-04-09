3cd9aabe16b7b55d053033baf6aba83c18451955
Gopi krishnan S <gopikrishnan1092@gmail.com>
2025-04-09 18:20:20 +0530

    two crystal ball

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,9 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpAmount;
+    for (; i < breaks.length; i += jumpAmount) if (breaks[i]) break;
+    let j = i - jumpAmount;
+    for (; j <= i; ++j) if (breaks[j]) return j;
 
+    return -1;
 }
