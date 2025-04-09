dfafa1d8be882deebfcd501fbb9690b803b1c966
Gopi krishnan S <gopikrishnan1092@gmail.com>
2025-04-09 12:12:24 +0530

    Binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,12 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length;
+    do {
+        const middle = Math.floor(low + (high - low) / 2);
+        const value = haystack[middle];
+        if (value === needle) return true;
+        else if (value < needle) low = middle + 1;
+        else high = middle;
+    } while (low < high);
+    return false;
 }
