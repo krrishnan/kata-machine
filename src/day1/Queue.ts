368ac5116bbcaf4a56602d4b6f2ce16790d6f1fc
Gopi krishnan S <gopikrishnan1092@gmail.com>
2025-04-09 21:24:57 +0530

    Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,33 @@
+type Node<T> = { value: T; next?: Node<T> };
 export default class Queue<T> {
     public length: number;
 
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const node: Node<T> = { value: item, next: undefined };
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
 
-}
+        this.tail.next = node;
+        this.tail = node;
+    }
     deque(): T | undefined {
-
-}
+        if (!this.head) return undefined;
+        const node = this.head;
+        this.head = this.head.next;
+        node.next = undefined;
+        return node.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
