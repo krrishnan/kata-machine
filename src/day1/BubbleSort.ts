a6115b036a4d0f08e3e7628aa77961c86ceb31d7
Gopi krishnan S <gopikrishnan1092@gmail.com>
2025-04-09 20:02:58 +0530

    bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,14 @@
 export default function bubble_sort(arr: number[]): void {
+    let end = arr.length - 1;
 
+    while (end > 0) {
+        for (let i = 0; i < end; ++i) {
+            if (arr[i] > arr[i + 1]) {
+                const temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        end -= 1;
+    }
 }
