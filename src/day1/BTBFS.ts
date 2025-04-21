8a7f2d4bcf1587914853755ff2e8d08deb898b36
Gopi krishnan S <gopikrishnan1092@gmail.com>
2025-04-21 17:30:40 +0530

    BFS

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,14 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue: (BinaryNode<number> | undefined)[] = [];
+    queue.push(head);
 
+    while (queue.length) {
+        const curr = queue.shift();
+        if (!curr) return false;
+        if (curr.value === needle) return true;
+
+        curr.left && queue.push(curr.left);
+        curr.right && queue.push(curr.right);
+    }
+    return false;
 }
