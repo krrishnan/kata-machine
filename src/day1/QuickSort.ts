0e3c0838297f955c1df5eea61c7bceb570a47d8f
Gopi krishnan S <gopikrishnan1092@gmail.com>
2025-04-21 12:42:55 +0530

    Quicksort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,28 @@
-export default function quick_sort(arr: number[]): void {
+function weakSort(arr: number[], low: number, high: number) {
+    let idx = low - 1;
+    const pivot = arr[high];
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+        }
+    }
 
+    idx++;
+    arr[high] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+function qs(arr: number[], low: number, high: number): void {
+    if (low >= high) return;
+    const pivotIndex = weakSort(arr, low, high);
+    qs(arr, low, pivotIndex - 1);
+    qs(arr, pivotIndex + 1, high);
+}
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
+    console.log("RESULT", arr);
 }
